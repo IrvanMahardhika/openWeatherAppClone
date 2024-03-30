0a6b5a1c3a3296ccde468d2b2a281ca29b04dc18
IrvanMahardhika <irvan.mahardhika@icloud.com>
2024-03-30 16:05:55 +0700

    init typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,36 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['react', 'sort-imports-es6-autofix', 'eslint-plugin-import'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    eqeqeq: 2,
+    'no-console': 2,
+    'prefer-const': 2,
+    'import/newline-after-import': 2,
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['single', 'multiple', 'all', 'none'],
+      },
+    ],
+    'react/jsx-uses-vars': 2,
+    'react/no-unused-prop-types': 2,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
